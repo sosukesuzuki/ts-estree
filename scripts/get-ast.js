a44ab34e1869d13e60e22d764b3ca4da976ddde6
sosukesuzuki <aosukeke@gmail.com>
2021-02-21 00:38:23 +0900

    Fix script

--- scripts/get-ast.js
+++ scripts/get-ast.js
@@ -13,15 +13,14 @@ if (isBabel) {
     errorRecovery: true,
     plugins: ["typescript", "estree"],
   });
-  delete program.interpreter;
-  const astStr = JSON.stringify(program);
+  const astStr = JSON.stringify(program.body[0]);
   execSync(
     `echo '${astStr}' | jq 'walk(if type == "object" and has("loc") then del(.loc, .start, .end)  else . end)'`,
     { shell: true, stdio: "inherit" }
   );
 } else {
   const { parse } = require("@typescript-eslint/typescript-estree");
-  const result = parse(code);
-  const astStr = JSON.stringify(result);
+  const program = parse(code);
+  const astStr = JSON.stringify(program.body[0]);
   execSync(`echo '${astStr}' | jq`, { shell: true, stdio: "inherit" });
 }
